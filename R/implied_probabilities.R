f65b3e191568b3dd320f7b0ddf0b0f793190ecba
Jonas Moss <jonas.gjertsen@gmail.com>
2019-11-02 22:07:45 +0100

    :cat2: Linting.
    lintr::lint called on both files. All issues fixed except an issue with cyclomatic complexity. Fixing this will involve breaking implied into more (internal) functions.

--- R/implied_probabilities.R
+++ R/implied_probabilities.R
@@ -1,72 +1,89 @@
 #' Implied probabilities from bookmaker odds
 #'
-#' This function calculate the implied probabilities from bookmaker odds in decimal format, using five
-#' different methods to account for overround in the odds.
+#' This function calculate the implied probabilities from bookmaker odds in
+#' decimal format, using five different methods to account for overround in the
+#' odds.
 #'
-#' Method "\code{basic}" is the simplest method, and computes the implied probabilities by
-#' dividing the inverted odds by the sum of the inverted odds.
+#' Method "\code{basic}" is the simplest method, and computes the implied
+#' probabilities by dividing the inverted odds by the sum of the inverted odds.
 #'
-#' Method "\code{shin}" uses the method by Shin (1991). This model assumes that there is a fraction of
-#' insider trading, and that the bookmakers tries to maximize their profits. In addition to providing
-#' implied probabilities, the method also gives an estimate of the proportion if inside trade. The method
-#' implemented here is based on the description in Štrumbelj (2014).
+#' Method "\code{shin}" uses the method by Shin (1991). This model assumes that
+#' there is a fraction of insider trading, and that the bookmakers tries to
+#' maximize their profits. In addition to providing implied probabilities,
+#' the method also gives an estimate of the proportion if inside trade.
+#' The method implemented here is based on the description in Štrumbelj (2014).
 #'
-#' Methods "\code{wpo}", "\code{or}" and "\code{power}" are from the Wisdom of the Crowds document (the updated version) by
-#' Joseph Buchdahl. The method "\code{or}" is originally by Cheung (2015), and the method "\code{power}" is there referred
-#' to as the logarithmic method.
+#' Methods "\code{wpo}", "\code{or}" and "\code{power}" are from the Wisdom of
+#' the Crowds document (the updated version) by Joseph Buchdahl. The method
+#' "\code{or}" is originally by Cheung (2015), and the method "\code{power}"
+#' is there referred to as the logarithmic method.
 #'
-#' @param odds Data frame, matrix or numeric vector of bookmaker odds. The odds must be in the
-#'    decimal format and be greater than or equal to 1.
+#' @param odds Data frame, matrix or numeric vector of bookmaker odds. The odds
+#'    must be in the decimal format and be greater than or equal to 1.
 #' @param method 	The method to be used. See ‘Details’. Can be abbreviated.
-#' @param normalize Logical. Some of the methods will give small rounding errors. If \code{TRUE},
-#'    a final normalization is applied to make the probabilities sum to 1.
+#' @param normalize Logical. Some of the methods will give small rounding
+#'    errors. If \code{TRUE}, a final normalization is applied to make the
+#'    probabilities sum to 1.
 #'
-#' @return A named list. The first component is named "\code{probabilities}"and contain a matrix of
-#' implied probabilities. The second in the bookmaker margins (aka the overround). The third
-#' depends on the method used to compute the probabilities:
+#' @return A named list. The first component is named "\code{probabilities}"
+#' and contain a matrix of implied probabilities. The second in the
+#' bookmaker margins (aka the overround). The third depends on the method
+#' used to compute the probabilities:
 #'
 #' \itemize{
-#'  \item{\code{zvalues} (\code{method} = "\code{shin}"): The estimated amount of insider trade.}
-#'  \item{\code{specific_margins} (\code{method = "\code{wpo}"): Matrix of the margins applied to each outcome.}
-#'  \item{\code{odds_ratios} (\code{method} = "\code{or}"): Numeric with the odds ratio that is used to convert true
-#'  probabilities to bookmaker probabilities.}
-#'  \item{\code{exponents} (\code{method} = "\code{power}"): The (inverse) exponents that is used to convert true
-#'  probabilities to bookmaker probabilities.}
+#'  \item{\code{zvalues} (\code{method} = "\code{shin}"): The estimated amount
+#'      of insider trade.}
+#'  \item{\code{specific_margins} (\code{method = "\code{wpo}"): Matrix of the
+#'      margins applied to each outcome.}}
+#'  \item{\code{odds_ratios} (\code{method} = "\code{or}"): Numeric with the
+#'      odds ratio that is used to convert true probabilities to bookmaker
+#'      probabilities.}
+#'  \item{\code{exponents} (\code{method} = "\code{power}"): The (inverse)
+#'      exponents that is used to convert true probabilities to bookmaker
+#'      probabilities.}
 #' }
 #'
-#' The fourth component \code{problematic} is a logical vector called indicating which, if any, probabilities has fallen
-#' outside the 0-1 range.
+#' The fourth component \code{problematic} is a logical vector called indicating
+#' which, if any, probabilities has fallen outside the 0-1 range.
 #'
 #' @references
 #'
-#'    Shin, H. S. (1991). Optimal betting odds against insider traders. Economic Journal, 101(408), 1179-1185.
+#'    Shin, H. S. (1991). Optimal betting odds against insider traders.
+#'      Economic Journal, 101(408), 1179-1185.
 #'
-#'    Štrumbelj, E. (2014). On determining probability forecasts from betting odds. International Journal of Forecasting, 30(4), 934-943.
+#'    Štrumbelj, E. (2014). On determining probability forecasts from betting
+#'      odds. International Journal of Forecasting, 30(4), 934-943.
 #'
-#'    Joseph Buchdahl - Using the wisdom of the crowd to find value in a football match betting market \url{http://www.football-data.co.uk/wisdom_of_crowd_bets}
+#'    Joseph Buchdahl - Using the wisdom of the crowd to find value in a
+#'      football match betting market
+#'      \url{http://www.football-data.co.uk/wisdom_of_crowd_bets}
 #'
-#'    Keith Cheung (2015) Fixed-odds betting and traditional odds \url{www.sportstradingnetwork.com/article/fixed-odds-betting-traditional-odds/}
+#'    Keith Cheung (2015) Fixed-odds betting and traditional odds
+#'      \url{www.sportstradingnetwork.com/article/fixed-odds-
+#'      betting-traditional-odds/}
 #'
 #' @export
 
-implied_probabilities <- function(odds, method = c("basic", "shin", "wpo", "or", "power", "additive"), normalize = TRUE){
+implied_probabilities <- function(odds, method = c("basic", "shin", "wpo", "or",
+                                                   "power", "additive"),
+                                  normalize = TRUE) {
 
   method <- match.arg(method)
   stopifnot(all(odds >= 1))
 
-  if (!is.matrix(odds)){
+  if (!is.matrix(odds)) {
 
-    if ('data.frame' %in% class(odds)){
+    if ("data.frame" %in% class(odds)) {
       odds <- as.matrix(odds)
     } else {
-      odds <- matrix(odds, nrow=1,
+      odds <- matrix(odds, nrow = 1,
                      dimnames = list(NULL, names(odds)))
     }
   }
 
   # Prepare the list that will be returned.
-  out <- vector(mode='list', length=2)
-  names(out) <- c('probabilities', 'margin')
+  out <- vector(mode = "list", length = 2)
+  names(out) <- c("probabilities", "margin")
 
   # Some useful quantities
   n_odds <- nrow(odds)
@@ -77,63 +94,70 @@ implied_probabilities <- function(odds, method = c("basic", "shin", "wpo", "or",
   inverted_odds_sum <- rowSums(inverted_odds)
   out$margin <- inverted_odds_sum - 1
 
-  if (method == 'basic'){
+  if (method == "basic") {
     out$probabilities <- inverted_odds / inverted_odds_sum
 
-  } else if (method == 'shin'){
+  } else if (method == "shin") {
 
     zvalues <- numeric(n_odds) # The proportion of insider trading.
-    probs <- matrix(nrow=n_odds, ncol=n_outcomes)
+    probs <- matrix(nrow = n_odds, ncol = n_outcomes)
 
-    for (ii in 1:n_odds){
-      res <- stats::uniroot(f=shin_solvefor, interval = c(0,0.4), io=inverted_odds[ii,])
+    for (ii in 1:n_odds) {
+
+      res <- stats::uniroot(f = shin_solvefor, interval = c(0, 0.4),
+                            io = inverted_odds[ii, ])
       zvalues[ii] <- res$root
-      probs[ii,] <- shin_func(zz=res$root, io = inverted_odds[ii,])
+      probs[ii, ] <- shin_func(zz = res$root, io = inverted_odds[ii, ])
+
     }
 
     out$probabilities <- probs
     out$zvalues <- zvalues
 
-  } else if (method == 'wpo'){
+  } else if (method == "wpo") {
     # Margin Weights Proportional to the Odds.
     # Method from the Wisdom of the Crowds pdf.
     fair_odds <- (n_outcomes * odds) / (n_outcomes - (out$margin * odds))
     out$probabilities <- 1 / fair_odds
-    out$specific_margins = (out$margin * fair_odds) / n_outcomes
-  } else if (method == 'or'){
+    out$specific_margins <- (out$margin * fair_odds) / n_outcomes
+
+  } else if (method == "or") {
 
     odds_ratios <- numeric(n_odds)
-    probs <- matrix(nrow=n_odds, ncol=n_outcomes)
+    probs <- matrix(nrow = n_odds, ncol = n_outcomes)
 
-    for (ii in 1:n_odds){
-      res <- stats::uniroot(f=or_solvefor, interval = c(0.05, 5), io=inverted_odds[ii,])
+    for (ii in 1:n_odds) {
+      res <- stats::uniroot(f = or_solvefor, interval = c(0.05, 5),
+                            io = inverted_odds[ii, ])
       odds_ratios[ii] <- res$root
-      probs[ii,] <- or_func(cc=res$root, io = inverted_odds[ii,])
+      probs[ii, ] <- or_func(cc = res$root, io = inverted_odds[ii, ])
     }
 
     out$probabilities <- probs
     out$odds_ratios <- odds_ratios
 
-  } else if (method == 'power'){
+  } else if (method == "power") {
 
-    probs <- matrix(nrow=n_odds, ncol=n_outcomes)
+    probs <- matrix(nrow = n_odds, ncol = n_outcomes)
     exponents <- numeric(n_odds)
 
-    for (ii in 1:n_odds){
-      res <- stats::uniroot(f=pwr_solvefor, interval = c(0.001, 1), io=inverted_odds[ii,])
+    for (ii in 1:n_odds) {
+      res <- stats::uniroot(f = pwr_solvefor, interval = c(0.001, 1),
+                            io  = inverted_odds[ii, ])
       exponents[ii] <- res$root
-      probs[ii,] <- pwr_func(nn=res$root, io = inverted_odds[ii,])
+      probs[ii, ] <- pwr_func(nn = res$root, io = inverted_odds[ii, ])
     }
 
     out$probabilities <- probs
     out$exponents <- exponents
 
-  } else if (method == 'additive'){
+  } else if (method == "additive") {
 
-    probs <- matrix(nrow=n_odds, ncol=n_outcomes)
+    probs <- matrix(nrow = n_odds, ncol = n_outcomes)
 
-    for (ii in 1:n_odds){
-      probs[ii,] <- inverted_odds[ii,] - ((inverted_odds_sum[ii] - 1) / n_outcomes)
+    for (ii in 1:n_odds) {
+      probs[ii, ] <- inverted_odds[ii, ] - ((inverted_odds_sum[ii] - 1)
+                                          / n_outcomes)
     }
 
     out$probabilities <- probs
@@ -142,23 +166,25 @@ implied_probabilities <- function(odds, method = c("basic", "shin", "wpo", "or",
 
   ## do a final normalization to make sure the probabilities
   ## sum to 1 without rounding errors.
-  if (normalize){
+  if (normalize) {
     out$probabilities <- out$probabilities / rowSums(out$probabilities)
   }
 
   # Make sure the matrix of implied probabilities has column names.
-  if (!is.null(colnames(odds))){
+  if (!is.null(colnames(odds))) {
     colnames(out$probabilities) <- colnames(odds)
   }
 
   # check if there are any probabilities outside the 0-1 range.
-  problematic <- apply(out$probabilities, MARGIN = 1, FUN=function(x){any(x > 1 | x < 0)})
-  if (any(problematic)){
-    warning(sprintf('Probabilities outside the 0-1 range produced at %d instances.\n',
-                    sum(problematic)))
+  problematic <- apply(out$probabilities,
+                       MARGIN = 1,
+                       FUN = function(x) any(x > 1 | x < 0))
+  if (any(problematic)) {
+    msg <- "Probabilities outside the 0-1 range produced at %d instances.\n"
+    warning(sprintf(msg, sum(problematic)))
   }
-  out$problematic <- problematic
 
+  out$problematic <- problematic
 
   return(out)
 }
--- R/internal.R
+++ R/internal.R
@@ -5,9 +5,9 @@
 #' @return Probabilities using Shin's formula
 #' @keywords internal
 
-shin_func <- function(zz, io){
+shin_func <- function(zz, io) {
   bb <- sum(io)
-  (sqrt(zz^2 + 4*(1 - zz) * (((io)^2)/bb)) - zz) / (2*(1-zz))
+  (sqrt(zz^2 + 4 * (1 - zz) * ((io ^ 2) / bb)) - zz) / (2 * (1 - zz))
 }
 
 #' The condition that the sum of the probabilites must sum to 1. Used with
@@ -18,7 +18,7 @@ shin_func <- function(zz, io){
 #' @return `0` if the probabilities sum to 1, `1` otherwise.
 #' @keywords internal
 
-shin_solvefor <- function(zz, io){
+shin_solvefor <- function(zz, io) {
   tmp <- shin_func(zz, io)
   1 - sum(tmp) # 0 when the condition is satisfied.
 }
@@ -31,8 +31,8 @@ shin_solvefor <- function(zz, io){
 #' @return Probabilities using the odds ratio method.
 #' @keywords internal
 
-or_func <- function(cc, io){
-  io / (cc + io - (cc*io))
+or_func <- function(cc, io) {
+  io / (cc + io - (cc * io))
 }
 
 #' The condition that the sum of the probabilites must sum to 1. This function
@@ -44,7 +44,7 @@ or_func <- function(cc, io){
 #' @return True probabilities.
 #' @keywords internal
 
-or_solvefor <- function(cc, io){
+or_solvefor <- function(cc, io) {
   tmp <- or_func(cc, io)
   sum(tmp) - 1
 }
@@ -56,9 +56,8 @@ or_solvefor <- function(cc, io){
 #' @return Numeric.
 #' @keywords internal
 
-pwr_func <- function(nn, io){
-  io^(1/nn)
-}
+pwr_func <- function(nn, io) io ^ (1 / nn)
+
 
 #' The condition that the sum of the probabilites must sum to 1. This function
 #' calulates the true probability, given bookmaker probabilites xx, and the
@@ -69,7 +68,4 @@ pwr_func <- function(nn, io){
 #' @return True probabilities.
 #' @keywords internal
 
-pwr_solvefor <- function(nn, io){
-  tmp <- pwr_func(nn, io)
-  sum(tmp) - 1
-}
+pwr_solvefor <- function(nn, io) sum(pwr_func(nn, io)) - 1
