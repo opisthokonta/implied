6f20a359e1adb0d5f6b6fc0f768e7f445f0201cc
Jonas Moss <jonas.gjertsen@gmail.com>
2019-11-02 22:20:11 +0100

    :cat2: Split implied_probabilities into two.

--- R/internal.R
+++ R/internal.R
@@ -1,3 +1,100 @@
+#' Handles the calculations in implied_probabilities
+#'
+#' @param odds Odds in a preprocessed format.
+#' @param method The method, already preprocessed from `implied_probabilities`.
+#' @return An `out` list for use `implied_probablities`.
+
+implied_handler <- function(odds, method = "method") {
+
+  # Prepare the list that will be returned.
+  out <- vector(mode = "list", length = 2)
+  names(out) <- c("probabilities", "margin")
+
+  # Some useful quantities
+  n_odds <- nrow(odds)
+  n_outcomes <- ncol(odds)
+
+  # Inverted odds and margins
+  inverted_odds <- 1 / odds
+  inverted_odds_sum <- rowSums(inverted_odds)
+  out$margin <- inverted_odds_sum - 1
+
+  if (method == "basic") {
+    out$probabilities <- inverted_odds / inverted_odds_sum
+
+  } else if (method == "shin") {
+
+    zvalues <- numeric(n_odds) # The proportion of insider trading.
+    probs <- matrix(nrow = n_odds, ncol = n_outcomes)
+
+    for (ii in 1:n_odds) {
+
+      res <- stats::uniroot(f = shin_solvefor, interval = c(0, 0.4),
+                            io = inverted_odds[ii, ])
+      zvalues[ii] <- res$root
+      probs[ii, ] <- shin_func(zz = res$root, io = inverted_odds[ii, ])
+
+    }
+
+    out$probabilities <- probs
+    out$zvalues <- zvalues
+
+  } else if (method == "wpo") {
+    # Margin Weights Proportional to the Odds.
+    # Method from the Wisdom of the Crowds pdf.
+    fair_odds <- (n_outcomes * odds) / (n_outcomes - (out$margin * odds))
+    out$probabilities <- 1 / fair_odds
+    out$specific_margins <- (out$margin * fair_odds) / n_outcomes
+
+  } else if (method == "or") {
+
+    odds_ratios <- numeric(n_odds)
+    probs <- matrix(nrow = n_odds, ncol = n_outcomes)
+
+    for (ii in 1:n_odds) {
+      res <- stats::uniroot(f = or_solvefor, interval = c(0.05, 5),
+                            io = inverted_odds[ii, ])
+      odds_ratios[ii] <- res$root
+      probs[ii, ] <- or_func(cc = res$root, io = inverted_odds[ii, ])
+    }
+
+    out$probabilities <- probs
+    out$odds_ratios <- odds_ratios
+
+  } else if (method == "power") {
+
+    probs <- matrix(nrow = n_odds, ncol = n_outcomes)
+    exponents <- numeric(n_odds)
+
+    for (ii in 1:n_odds) {
+      res <- stats::uniroot(f = pwr_solvefor, interval = c(0.001, 1),
+                            io  = inverted_odds[ii, ])
+      exponents[ii] <- res$root
+      probs[ii, ] <- pwr_func(nn = res$root, io = inverted_odds[ii, ])
+    }
+
+    out$probabilities <- probs
+    out$exponents <- exponents
+
+  } else if (method == "additive") {
+
+    probs <- matrix(nrow = n_odds, ncol = n_outcomes)
+
+    for (ii in 1:n_odds) {
+      probs[ii, ] <- inverted_odds[ii, ] - ((inverted_odds_sum[ii] - 1)
+                                            / n_outcomes)
+    }
+
+    out$probabilities <- probs
+
+  }
+
+  out
+
+}
+
+
+
 #' Calculate the probabilities using Shin's formula, for a given value of z.
 #'
 #' @param zz ?
