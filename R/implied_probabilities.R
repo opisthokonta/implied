c7f226d4b933a0bad6c253bccbffc4018f64ab0d
Jonas <jonaslindstrom88@gmail.com>
2019-11-22 19:35:57 +0100

    version 0.2.0 submitted to cran

--- R/implied_probabilities.R
+++ R/implied_probabilities.R
@@ -2,36 +2,47 @@
 
 #' Implied probabilities from bookmaker odds.
 #'
-#' This function calculate the implied probabilties from bookmaker odds in decimal format, using five
-#' different methods to account for overround in the odds.
+#' This function calculate the implied probabilties from bookmaker odds in decimal format, while
+#' accounting for overround in the odds.
 #'
 #' The method 'basic' is the simplest method, and computes the implied probabilities by
 #' dividing the inverted odds by the sum of the inverted odds.
 #'
-#' The method 'shin' uses the method by Shin (1991). This model assumes that there is a fraction of
+#' The method 'shin' uses the method by Shin (1992, 1993). This model assumes that there is a fraction of
 #' insider trading, and that the bookmakers tries to maximize their profits. In addition to providing
 #' implied probabilties, the method also gives an estimate of the proportion if inside trade. The method
-#' implemented here is based on the description in Štrumbelj (2014).
+#' implemented here is based on the algorithm in Jullien & Salanié (1994).
 #'
-#' The methods 'wpo', 'or' and 'power' are form the Wisdom of the Crowds document (the updated version) by
+#' The 'bb' (short for "balanced books") method is from Fingleton & Waldron (1999), and is a variant of Shin's method. It too assume
+#' a fraction of insiders, but instead of assuming that the bookmakers maximize their profits, they
+#' minimize their risk.
+#'
+#' Both the 'shin' and 'bb' methods can be used together with the 'grossmargin' argument. This is also
+#' from the Fingleton & Waldron (1999) paper, and adds some further assumption to the calculations,
+#' related to opperating costs. grossmargin should be 0 (default) or greater, typical range is 0 to 0.05.
+#' For values other than 0, this might sometimes cause some probabilities to not be identifiable. A warning
+#' will be given if this happens.
+#'
+#' The methods 'wpo' (Weights Proportional to the Odds), 'or' (Odds Ratio) and 'power' are form the Wisdom of the Crowds document (the updated version) by
 #' Joseph Buchdahl. The method 'or' is origianlly by Cheung (2015), and the method 'power' is there referred
 #' to as the logarithmic method.
 #'
 #'
 #'
 #' @param odds A matrix or numeric of bookmaker odds. The odds must be in the decimal format.
-#' @param method A string giving the method to use. Valid methods are 'basic', 'shin',
+#' @param method A string giving the method to use. Valid methods are 'basic', 'shin', 'bb',
 #' 'wpo', 'or', 'power' or 'additive'.
 #' @param normalize Logical. Some of the methods will give small rounding errors. If TRUE (default)
 #' a final normalization is applied to make absoultely sure the
 #' probabilities sum to 1.
+#' @param grossmargin Numeric. Must be 0 or greater. See the details.
 #'
 #'
 #' @return A named list. The first component is named 'probabilities' and contain a matrix of
 #' implied probabilities. The second in the bookmaker margins (aka the overround). The third
 #' depends on the method used to compute the probabilities:
 #' \itemize{
-#'  \item{zvalues (method = 'shin'): The estimated amount of insider trade.}
+#'  \item{ zvalues (method = 'shin' and method='bb'): The estimated amount of insider trade.}
 #'  \item{ specific_margins (method = 'wpo'): Matrix of the margins applied to each outcome.}
 #'  \item{ odds_ratios (method = 'or'): Numeric with the odds ratio that is used to convert true
 #'  probabilities to bookamker probabilties.}
@@ -40,22 +51,25 @@
 #' }
 #'
 #' The fourth compnent 'problematic' is a logical vector called indicating if any probabilites has fallen
-#' outside the 0-1 range.
+#' outside the 0-1 range, or if there were some other problem computing the probabilities.
 #'
 #'
 #' @section References:
 #' \itemize{
-#'  \item{Hyun Song Shin (1991) Optimal betting odds against insider traders.}
-#'  \item{Erik Štrumbelj (2014) On determining probability forecasts from betting odds.}
+#'  \item{Hyun Song Shin (1992) Prices of State Contingent Claims with Insider Traders, and the Favourite-Longshot Bias }
+#'  \item{Hyun Song Shin (1993) Measuring the Incidence of Insider Trading in a Market for State-Contingent Claims}
+#'  \item{Bruno Jullien & Bernard Salanié (1994) Measuring the incidence of insider trading: A comment on Shin.}
+#'  \item{John Fingleton & Patrick Waldron (1999) Optimal Determination of Bookmakers' Betting Odds: Theory and Tests.}
 #'  \item{Joseph Buchdahl - USING THE WISDOM OF THE CROWD TO FIND VALUE IN A FOOTBALL MATCH BETTING MARKET (http://www.football-data.co.uk/wisdom_of_crowd_bets)}
 #'  \item{Keith Cheung (2015) Fixed-odds betting and traditional odds (www.sportstradingnetwork.com/article/fixed-odds-betting-traditional-odds/)}
 #' }
 #' @export
-implied_probabilities <- function(odds, method='basic', normalize=TRUE){
+implied_probabilities <- function(odds, method='basic', normalize=TRUE, grossmargin = 0){
 
   stopifnot(length(method) == 1,
-            tolower(method) %in% c('basic', 'shin', 'wpo', 'or', 'power', 'additive'),
-            all(odds >= 1))
+            tolower(method) %in% c('basic', 'shin', 'bb', 'wpo', 'or', 'power', 'additive'),
+            all(odds >= 1),
+            grossmargin >= 0)
 
   if (!is.matrix(odds)){
 
@@ -88,27 +102,49 @@ implied_probabilities <- function(odds, method='basic', normalize=TRUE){
     zvalues <- numeric(n_odds) # The proportion of insider trading.
     probs <- matrix(nrow=n_odds, ncol=n_outcomes)
 
+    problematic_shin <- logical(n_odds)
     for (ii in 1:n_odds){
 
       # initialize zz at 0
       zz_tmp <- 0
+
       for (jj in 1:1000){
         zz_prev <- zz_tmp
-        zz_tmp <- (sum(sqrt(zz_prev^2 + 4*(1 - zz_prev) * (((inverted_odds[ii,])^2)/inverted_odds_sum[ii])))-2) / (n_outcomes - 2)
+
+        if (grossmargin != 0){
+          zz_tmp <- (sum(sqrt(zz_prev^2 + 4*(1 - zz_prev) * (((inverted_odds[ii,]^2 * (1-grossmargin)))/inverted_odds_sum[ii])))-2) / (n_outcomes - 2)
+        } else {
+          zz_tmp <- (sum(sqrt(zz_prev^2 + 4*(1 - zz_prev) * (((inverted_odds[ii,])^2)/inverted_odds_sum[ii])))-2) / (n_outcomes - 2)
+        }
 
         if (abs(zz_tmp - zz_prev)  <= .Machine$double.eps^0.25){
           break
-        } else if (ii >= 1000){
-          warning('Could not find z: Did not converge. The results may be unreliable')
+        } else if (jj >= 1000){
+          problematic_shin[ii] <- TRUE
         }
+
+        zvalues[ii] <- zz_tmp
+        probs[ii,] <- shin_func(zz=zz_tmp, io = inverted_odds[ii,])
       }
 
-      zvalues[ii] <- zz_tmp
-      probs[ii,] <- shin_func(zz=zz_tmp, io = inverted_odds[ii,])
+      out$probabilities <- probs
+      out$zvalues <- zvalues
+
     }
 
+    if (any(problematic_shin)){
+      warning(sprintf('Could not find z: Did not converge in %d instances. Some results may be unreliable. See the "problematic" vector in the output.',
+                      sum(problematic_shin)))
+    }
+
+  } else if (method == 'bb'){
+
+    zz <- (((1-grossmargin)*inverted_odds_sum) - 1) / (n_outcomes-1)
+    probs <- (((1-grossmargin) * inverted_odds) - zz) / (1-zz)
+
     out$probabilities <- probs
-    out$zvalues <- zvalues
+    out$zvalues <- zz
+
 
   } else if (method == 'wpo'){
     # Margin Weights Proportional to the Odds.
@@ -173,6 +209,19 @@ implied_probabilities <- function(odds, method='basic', normalize=TRUE){
     warning(sprintf('Probabilities outside the 0-1 range produced at %d instances.\n',
                     sum(problematic)))
   }
+
+  if (method == 'shin'){
+    problematic <- problematic | problematic_shin
+  }
+
+  if (method %in% c('shin', 'bb')){
+    negative_z <- out$zvalues < 0
+    if (any(negative_z)){
+      warning(sprintf('z estimated to be negative: Some results may be unreliable. See the "problematic" vector in the output.',
+                      negative_z))
+    }
+  }
+
   out$problematic <- problematic
 
 
--- tests/testthat/test_1.R
+++ tests/testthat/test_1.R
@@ -6,7 +6,7 @@ my_odds <- rbind(c(4.20, 3.70, 1.95),
                  c(2.05, 3.20, 3.80))
 
 # Some odds desinged to be problematic with the additive method.
-# It is also probelmtic with the wpo method.
+# It is also problematic with the wpo method.
 my_odds2 <- t(matrix(1/c(0.870, 0.2, 0.1, 0.05, 0.02, 0.01)))
 
 
@@ -14,6 +14,9 @@ context("Implied probabilities")
 
 iprobs1_basic <- implied_probabilities(my_odds)
 iprobs1_shin <- implied_probabilities(my_odds, method='shin')
+iprobs1_shin2 <- implied_probabilities(my_odds, method='shin', grossmargin = 0.01)
+iprobs1_bb <- implied_probabilities(my_odds, method='bb')
+iprobs1_bb2 <- implied_probabilities(my_odds, method='bb', grossmargin = 0.01)
 iprobs1_wpo <- implied_probabilities(my_odds, method='wpo')
 iprobs1_or <- implied_probabilities(my_odds, method='or')
 iprobs1_power <- implied_probabilities(my_odds, method='power')
@@ -34,6 +37,11 @@ test_that("Output", {
   expect_silent(
     iprobs2_shin <- implied_probabilities(my_odds2, method='shin')
   )
+
+  expect_silent(
+    iprobs2_shin2 <- implied_probabilities(my_odds2, method='shin', grossmargin = 0.01)
+  )
+
   expect_silent(
     iprobs2_basic <- implied_probabilities(my_odds2)
   )
@@ -55,6 +63,9 @@ test_that("Output", {
 
   expect_equal(class(iprobs1_basic), 'list')
   expect_equal(class(iprobs1_shin), 'list')
+  expect_equal(class(iprobs1_shin2), 'list')
+  expect_equal(class(iprobs1_bb), 'list')
+  expect_equal(class(iprobs1_bb2), 'list')
   expect_equal(class(iprobs1_wpo), 'list')
   expect_equal(class(iprobs1_or), 'list')
   expect_equal(class(iprobs1_additive), 'list')
@@ -62,6 +73,9 @@ test_that("Output", {
 
   expect_equal(all(abs(rowSums(iprobs1_basic$probabilities) - 1) < toll), TRUE)
   expect_equal(all(abs(rowSums(iprobs1_shin$probabilities) - 1) < toll), TRUE)
+  expect_equal(all(abs(rowSums(iprobs1_shin2$probabilities) - 1) < toll), TRUE)
+  expect_equal(all(abs(rowSums(iprobs1_bb$probabilities) - 1) < toll), TRUE)
+  expect_equal(all(abs(rowSums(iprobs1_bb2$probabilities) - 1) < toll), TRUE)
   expect_equal(all(abs(rowSums(iprobs1_wpo$probabilities) - 1) < toll), TRUE)
   expect_equal(all(abs(rowSums(iprobs1_or$probabilities) - 1) < toll), TRUE)
   expect_equal(all(abs(rowSums(iprobs1_power$probabilities) - 1) < toll), TRUE)
@@ -69,12 +83,18 @@ test_that("Output", {
 
   expect_equal(all(iprobs1_basic$margin > 0), TRUE)
   expect_equal(all(iprobs1_shin$margin > 0), TRUE)
+  expect_equal(all(iprobs1_shin2$margin > 0), TRUE)
+  expect_equal(all(iprobs1_bb$margin > 0), TRUE)
+  expect_equal(all(iprobs1_bb2$margin > 0), TRUE)
   expect_equal(all(iprobs1_wpo$margin > 0), TRUE)
   expect_equal(all(iprobs1_or$margin > 0), TRUE)
   expect_equal(all(iprobs1_power$margin > 0), TRUE)
   expect_equal(all(iprobs1_additive$margin > 0), TRUE)
 
   expect_equal(is.null(iprobs1_shin$zvalues), FALSE)
+  expect_equal(is.null(iprobs1_shin2$zvalues), FALSE)
+  expect_equal(is.null(iprobs1_bb$zvalues), FALSE)
+  expect_equal(is.null(iprobs1_bb2$zvalues), FALSE)
   expect_equal(is.null(iprobs1_wpo$specific_margins), FALSE)
   expect_equal(is.null(iprobs1_or$odds_ratios), FALSE)
   expect_equal(is.null(iprobs1_power$exponents), FALSE)
@@ -83,6 +103,7 @@ test_that("Output", {
 
   expect_equal(class(iprobs2_basic), 'list')
   expect_equal(class(iprobs2_shin), 'list')
+  expect_equal(class(iprobs2_shin2), 'list')
   expect_equal(class(iprobs2_wpo), 'list')
   expect_equal(class(iprobs2_or), 'list')
   expect_equal(class(iprobs2_additive), 'list')
